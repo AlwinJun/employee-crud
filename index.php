54b96fd764780b534c2653e2b386fd196e3a2a2b
AJ <mahidlawonaljun@gmail.com>
2022-10-21 19:24:37 +0800

    Update header and rename folders

--- index.php
+++ index.php
@@ -6,14 +6,22 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>UWP</title>
+  <!-- FONT ICONS -->
+  <script src="https://kit.fontawesome.com/25c871887e.js" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" href="dist/style.css">
 </head>
 
 <body>
-  <header>
-    <h1>UWP</h1>
-    <div class="user">
-      <p>ADMIN ALWIN</p>
-      <button class="log-icon"></button>
+  <header class="header">
+    <div class="header-left">
+      <i class="fa-solid fa-user-graduate header-logo"></i>
+      <h1>UWP</h1>
+    </div>
+    <div class="header-right">
+      <button class="header-notif"><i class="fa-solid fa-bell header-notify" data-number="3+"></i></button>
+      <p class="header-user">Admin Alwin</p>
+      <button class="header-icon"><i class="fa-regular fa-user"></i></button>
     </div>
   </header>
   <main>
