ebfc7f19b803261af9a9167cc437cb93f337f1f1
AJ <mahidlawonaljun@gmail.com>
2022-10-21 15:26:05 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>UWP</title>
+</head>
+
+<body>
+  <header>
+    <h1>UWP</h1>
+    <div class="user">
+      <p>ADMIN ALWIN</p>
+      <button class="log-icon"></button>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+      <section class="main-section">
+        <div class="top-border">
+          <h2>
+            MANAGE <span>STUDENTS</span>
+          </h2>
+          <div class="btns">
+            <button class="btn btn-add">Add student</button>
+            <button class="btn btn-refresh">Refresh table</button>
+            <button class="btn btn-delete">Delete</button>
+          </div>
+      </section>
+    </div>
+    </div>
+  </main>
+</body>
+
+</html>
