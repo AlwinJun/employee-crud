b53bcde51a4b5aa093e22006b86b07acceae40d1
AJ <mahidlawonaljun@gmail.com>
2022-11-16 09:03:41 +0800

    Add footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,9 @@
  </main>
-
+ <footer>
+   <p>
+     <a href="https://github.com/AlwinJun/employee-crud.git">CRUD System</a> requirement for IM101. Coded by <a href="https://github.com/AlwinJun"> Alwin Jun Mahidlawon.</a>
+   </p>
+ </footer>
  <script src="src/js/main.js"></script>
  </body>
 
